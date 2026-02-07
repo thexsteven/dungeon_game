abb7f500fe8d8aea078df27f3dd44743a698fa60
resolve_test <ste.braun.25@lehre.mosbach.dhbw.de>
2026-02-07 11:53:28 +0100

    Add Monster class with health and strength attributes

--- Monster.py
+++ Monster.py
@@ -1,5 +1,13 @@
+import random
+
 # Monster constants
 MIN_MONSTER_HEALTH = 40
 MAX_MONSTER_HEALTH = 80
 MIN_MONSTER_STRENGTH = 20
 MAX_MONSTER_STRENGTH = 40
+
+class Monster:
+  def __init__(self, name):
+    self.name = name
+    self.health = random.randint(MIN_MONSTER_HEALTH, MAX_MONSTER_HEALTH)
+    self.strength = random.randint(MIN_MONSTER_STRENGTH, MAX_MONSTER_STRENGTH)
