434a593ec5d9d3d53f48bdcb5f14d25665849e5c
resolve_test <ste.braun.25@lehre.mosbach.dhbw.de>
2026-02-07 14:34:14 +0100

    Implement start method in Game class for player interaction and room creation

--- Game.py
+++ Game.py
@@ -5,4 +5,18 @@ class Game:
     self.player = None
     self.RoomManager = None
 
+  def start(self):
+    player_name = input("Enter your player's name: ")
+    print(f"Welcome {player_name}, to the Dungeon Game!")
+
+    while True:
+      try:
+        num_rooms = int(input("How many rooms do you want to create? "))
+        if num_rooms <= 0:
+          print("Enter a number bigger than 0.")
+          continue
+        break
+      except ValueError:
+        print("Please enter a valid number")
+
   
