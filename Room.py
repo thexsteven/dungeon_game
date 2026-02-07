3f0175c7957710b9d41d535de7da6020690ae308
resolve_test <ste.braun.25@lehre.mosbach.dhbw.de>
2026-02-07 12:29:55 +0100

    Add fight_monster method to Room class for player-monster interactions

--- Room.py
+++ Room.py
@@ -5,3 +5,8 @@ class Room:
 
   def get_status(self):
     return f"Room: {self.name}, {self.monster.get_status()}"
+  
+  def fight_monster(self, player):
+    player.take_damage(self.monster.attack()) 
+    damage_dealt = player.attack() # den durch den Spieler verursachten Schaden speichern
+    self.monster.take_damage(damage_dealt)
