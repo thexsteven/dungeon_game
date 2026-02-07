92a5a6b95cacb167373a0d1adf8661915d538435
resolve_test <ste.braun.25@lehre.mosbach.dhbw.de>
2026-02-07 14:08:25 +0100

    Add is_game_over method to RoomManager for game state checking

--- RoomManager.py
+++ RoomManager.py
@@ -15,3 +15,6 @@ class RoomManager:
       print(f"\n Moved to room {self.current_room_index + 1}: {self.get_current_room().name}")
     else:
       print("Congratulations! You won the Dungeon game :)")
+
+  def is_game_over(self):
+    return self.is_game_over
