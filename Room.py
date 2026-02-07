8d4d24d7d5d38b194adb239ae33205f659be6ec3
resolve_test <ste.braun.25@lehre.mosbach.dhbw.de>
2026-02-07 13:30:28 +0100

    Enhance fight_monster method to allow players to regain health upon defeating a monster

--- Room.py
+++ Room.py
@@ -10,3 +10,6 @@ class Room:
     player.take_damage(self.monster.attack()) 
     damage_dealt = player.attack() # den durch den Spieler verursachten Schaden speichern
     self.monster.take_damage(damage_dealt)
+    if not self.monster.is_alive():
+      health_gained = int(damage_dealt/2) 
+      player.regain_health(health_gained)
