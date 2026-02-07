64c216a827c38ce174c5a21466a2b8a991ef2023
resolve_test <ste.braun.25@lehre.mosbach.dhbw.de>
2026-02-07 13:59:27 +0100

    Add get_current_room method to RoomManager for retrieving the current room

--- Level.py
+++ Level.py
@@ -2,3 +2,9 @@ class RoomManager:
   def __init__(self, rooms):
     self.rooms = rooms
     self.current_room_index = 0
+
+  def get_current_room(self):
+    if self.current_room_index < len(self.rooms):
+      return self.rooms[self.current_room_index]
+    else:
+      return None
