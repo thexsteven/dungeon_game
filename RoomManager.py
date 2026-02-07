89431ab3dac97bc1b81d004960da481b634aaf14
resolve_test <ste.braun.25@lehre.mosbach.dhbw.de>
2026-02-07 14:20:41 +0100

    Fix is_game_over method in RoomManager to return the correct game_over state

--- RoomManager.py
+++ RoomManager.py
@@ -2,6 +2,7 @@ class RoomManager:
   def __init__(self, rooms):
     self.rooms = rooms
     self.current_room_index = 0
+    #self.game_over = False
 
   def get_current_room(self):
     if self.current_room_index < len(self.rooms):
@@ -17,4 +18,4 @@ class RoomManager:
       print("Congratulations! You won the Dungeon game :)")
 
   def is_game_over(self):
-    return self.is_game_over
+    return self.game_over
